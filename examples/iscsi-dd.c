ca1e152f2c47890b284cb51a741b08e6f8f85f2c
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2011-01-23 18:35:58 +1100

    New example file : iscsi-dd can be used to copy the content of one iscsi lun onto a different iscsi lun of the exact same sixe.
    This example  illustrates how to "steal" a task structure from a callback
    so we can use store it and have it remains valid after the read10 callback
    has completed.
    It also illustrates the async api of libiscsi for read10/write10.
    
    Initially a number of read10 calls are made asynchronously until the
    queue is full.
    As each read10 completes and returns data, we issue a write10 to write
    that data to the other lun.
    
    As soon as a write10 completes, we "release" the initial read10 task corresponding to what we wrote and issue a new read10 to continue copying the next set of un-read data.
    
    Using the async api, it should be easy to get very high performance and
    throughput even from one single thread.

---
+++ examples/iscsi-dd.c
@@ -0,0 +1,298 @@
+/* 
+   Copyright (C) 2010 by Ronnie Sahlberg <ronniesahlberg@gmail.com>
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <poll.h>
+#include <popt.h>
+#include "iscsi.h"
+#include "scsi-lowlevel.h"
+
+char *initiator = "iqn.2010-11.ronnie:iscsi-inq";
+int max_in_flight = 50;
+int blocks_per_io = 200;
+
+struct client {
+	int finished;
+	int in_flight;
+
+	struct iscsi_context *src_iscsi;
+	int src_lun;
+	int src_blocksize;
+	uint64_t src_num_blocks;
+	uint64_t pos;
+
+	struct iscsi_context *dst_iscsi;
+	int dst_lun;
+	int dst_blocksize;
+	uint64_t dst_num_blocks;
+};
+
+
+void fill_read_queue(struct client *client);
+
+struct write_task {
+       struct scsi_task *rt;
+       struct client *client;
+};
+
+void write10_cb(struct iscsi_context *iscsi, int status, void *command_data, void *private_data)
+{
+	struct write_task *wt = (struct write_task *)private_data;
+	struct scsi_task *task = command_data;
+	struct client *client = wt->client;
+
+	if (status == SCSI_STATUS_CHECK_CONDITION) {
+		printf("Write10 failed with sense key:%d ascq:%04x\n", task->sense.key, task->sense.ascq);
+		exit(10);
+	}
+	if (status != SCSI_STATUS_GOOD) {
+		printf("Write10 failed with %s\n", iscsi_get_error(iscsi));
+		exit(10);
+	}
+
+	client->in_flight--;
+	fill_read_queue(client);
+
+	if ((client->in_flight == 0) && (client->pos == client->src_num_blocks)) {
+		client->finished = 1;
+	}
+	scsi_free_scsi_task(wt->rt);
+	free(wt);
+}
+
+void read10_cb(struct iscsi_context *iscsi, int status, void *command_data, void *private_data)
+{
+	struct client *client = (struct client *)private_data;
+	struct scsi_task *task = command_data;
+	struct write_task *wt;
+
+	if (status == SCSI_STATUS_CHECK_CONDITION) {
+		printf("Read10 failed with sense key:%d ascq:%04x\n", task->sense.key, task->sense.ascq);
+		exit(10);
+	}
+
+	wt = malloc(sizeof(struct write_task));
+	iscsi_cbdata_steal_scsi_task(task);
+	wt->rt = task;
+	wt->client = client;
+
+	if (iscsi_write10_async(client->dst_iscsi,
+			client->dst_lun,
+			task->datain.data,
+			task->datain.size,
+			task->params.read10.lba,
+			0, 0,
+			client->dst_blocksize, write10_cb,
+			wt) != 0) {
+		printf("failed to send read10 command\n");
+		exit(10);
+	}
+}
+
+
+void fill_read_queue(struct client *client)
+{
+	int num_blocks;
+
+	while(client->in_flight < max_in_flight && client->pos < client->src_num_blocks) {
+		client->in_flight++;
+
+		num_blocks = client->src_num_blocks - client->pos;
+		if (num_blocks > blocks_per_io) {
+			num_blocks = blocks_per_io;
+		}
+
+		if (iscsi_read10_async(client->src_iscsi,
+				client->src_lun, client->pos,
+				num_blocks * client->src_blocksize,
+				client->src_blocksize, read10_cb, client) != 0) {
+			printf("failed to send read10 command\n");
+			exit(10);
+		}
+		client->pos += num_blocks;
+	}
+}
+
+int main(int argc, const char *argv[])
+{
+	poptContext pc;
+	const char **extra_argv;
+	char *src_url = NULL;
+	char *dst_url = NULL;
+	struct iscsi_url *iscsi_url;
+	struct scsi_task *task;
+	struct scsi_readcapacity10 *rc10;
+	int extra_argc = 0;
+	int res;
+	struct pollfd pfd[2];
+	struct client client;
+
+	struct poptOption popt_options[] = {
+		POPT_AUTOHELP
+		{ "initiator-name", 'i', POPT_ARG_STRING, &initiator, 0, "Initiatorname to use", "iqn-name" },
+		{ "src", 0, POPT_ARG_STRING, &src_url, 0, "SRC lun", "iscsi url" },
+		{ "dst", 0, POPT_ARG_STRING, &dst_url, 0, "DST lun", "iscsi url" },
+		POPT_TABLEEND
+	};
+
+	pc = poptGetContext(argv[0], argc, argv, popt_options, POPT_CONTEXT_POSIXMEHARDER);
+	if ((res = poptGetNextOpt(pc)) < -1) {
+		fprintf(stderr, "Failed to parse option : %s %s\n",
+			poptBadOption(pc, 0), poptStrerror(res));
+		exit(10);
+	}
+	extra_argv = poptGetArgs(pc);
+	poptFreeContext(pc);
+
+	if (src_url == NULL) {
+		fprintf(stderr, "You must specify source url\n");
+		fprintf(stderr, "  --src iscsi://<host>[:<port>]/<target-iqn>/<lun>\n", argv[0]);
+		exit(10);
+	}
+	if (dst_url == NULL) {
+		fprintf(stderr, "You must specify destination url\n");
+		fprintf(stderr, "  --dst iscsi://<host>[:<port>]/<target-iqn>/<lun>\n", argv[0]);
+		exit(10);
+	}
+
+
+	memset(&client, 0, sizeof(client));
+
+
+	client.src_iscsi = iscsi_create_context(initiator);
+	if (client.src_iscsi == NULL) {
+		fprintf(stderr, "Failed to create context\n");
+		exit(10);
+	}
+	iscsi_url = iscsi_parse_full_url(client.src_iscsi, src_url);
+	if (iscsi_url == NULL) {
+		fprintf(stderr, "Failed to parse URL: %s\n", 
+			iscsi_get_error(client.src_iscsi));
+		exit(10);
+	}
+	iscsi_set_targetname(client.src_iscsi, iscsi_url->target);
+	iscsi_set_session_type(client.src_iscsi, ISCSI_SESSION_NORMAL);
+	iscsi_set_header_digest(client.src_iscsi, ISCSI_HEADER_DIGEST_NONE_CRC32C);
+	if (iscsi_url->user != NULL) {
+		if (iscsi_set_initiator_username_pwd(client.src_iscsi, iscsi_url->user, iscsi_url->passwd) != 0) {
+			fprintf(stderr, "Failed to set initiator username and password\n");
+			exit(10);
+		}
+	}
+	if (iscsi_full_connect_sync(client.src_iscsi, iscsi_url->portal, iscsi_url->lun) != 0) {
+		fprintf(stderr, "Login Failed. %s\n", iscsi_get_error(client.src_iscsi));
+		iscsi_destroy_url(iscsi_url);
+		iscsi_destroy_context(client.src_iscsi);
+		exit(10);
+	}
+	client.src_lun = iscsi_url->lun;
+	iscsi_destroy_url(iscsi_url);
+
+	task = iscsi_readcapacity10_sync(client.src_iscsi, client.src_lun, 0, 0);
+	if (task == NULL || task->status != SCSI_STATUS_GOOD) {
+		fprintf(stderr, "failed to send readcapacity command\n");
+		exit(10);
+	}
+	rc10 = scsi_datain_unmarshall(task);
+	if (rc10 == NULL) {
+		fprintf(stderr, "failed to unmarshall readcapacity10 data\n");
+		exit(10);
+	}
+	client.src_blocksize  = rc10->block_size;
+	client.src_num_blocks  = rc10->lba;
+	scsi_free_scsi_task(task);
+
+
+
+
+
+	client.dst_iscsi = iscsi_create_context(initiator);
+	if (client.dst_iscsi == NULL) {
+		fprintf(stderr, "Failed to create context\n");
+		exit(10);
+	}
+	iscsi_url = iscsi_parse_full_url(client.dst_iscsi, dst_url);
+	if (iscsi_url == NULL) {
+		fprintf(stderr, "Failed to parse URL: %s\n", 
+			iscsi_get_error(client.dst_iscsi));
+		exit(10);
+	}
+	iscsi_set_targetname(client.dst_iscsi, iscsi_url->target);
+	iscsi_set_session_type(client.dst_iscsi, ISCSI_SESSION_NORMAL);
+	iscsi_set_header_digest(client.dst_iscsi, ISCSI_HEADER_DIGEST_NONE_CRC32C);
+	if (iscsi_url->user != NULL) {
+		if (iscsi_set_initiator_username_pwd(client.dst_iscsi, iscsi_url->user, iscsi_url->passwd) != 0) {
+			fprintf(stderr, "Failed to set initiator username and password\n");
+			exit(10);
+		}
+	}
+	if (iscsi_full_connect_sync(client.dst_iscsi, iscsi_url->portal, iscsi_url->lun) != 0) {
+		fprintf(stderr, "Login Failed. %s\n", iscsi_get_error(client.dst_iscsi));
+		iscsi_destroy_url(iscsi_url);
+		iscsi_destroy_context(client.dst_iscsi);
+		exit(10);
+	}
+	client.dst_lun = iscsi_url->lun;
+	iscsi_destroy_url(iscsi_url);
+
+	task = iscsi_readcapacity10_sync(client.dst_iscsi, client.dst_lun, 0, 0);
+	if (task == NULL || task->status != SCSI_STATUS_GOOD) {
+		fprintf(stderr, "failed to send readcapacity command\n");
+		exit(10);
+	}
+	rc10 = scsi_datain_unmarshall(task);
+	if (rc10 == NULL) {
+		fprintf(stderr, "failed to unmarshall readcapacity10 data\n");
+		exit(10);
+	}
+	client.dst_blocksize  = rc10->block_size;
+	client.dst_num_blocks  = rc10->lba;
+	scsi_free_scsi_task(task);
+
+	fill_read_queue(&client);
+
+	while (client.finished == 0) {
+		pfd[0].fd = iscsi_get_fd(client.src_iscsi);
+		pfd[0].events = iscsi_which_events(client.src_iscsi);
+		pfd[1].fd = iscsi_get_fd(client.dst_iscsi);
+		pfd[1].events = iscsi_which_events(client.dst_iscsi);
+
+		if (poll(&pfd[0], 2, -1) < 0) {
+			printf("Poll failed");
+			exit(10);
+		}
+		if (iscsi_service(client.src_iscsi, pfd[0].revents) < 0) {
+			printf("iscsi_service failed with : %s\n", iscsi_get_error(client.src_iscsi));
+			break;
+		}
+		if (iscsi_service(client.dst_iscsi, pfd[1].revents) < 0) {
+			printf("iscsi_service failed with : %s\n", iscsi_get_error(client.dst_iscsi));
+			break;
+		}
+	}
+
+	iscsi_logout_sync(client.src_iscsi);
+	iscsi_destroy_context(client.src_iscsi);
+	iscsi_logout_sync(client.dst_iscsi);
+	iscsi_destroy_context(client.dst_iscsi);
+
+	return 0;
+}
+
