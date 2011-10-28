17319072b192c327012626818465ab9115a20c42
Paolo Bonzini <pbonzini@redhat.com>
2011-10-28 14:29:43 +0200

    use automake and libtool
    This commits replaces the handwritten makefile with automake+libtool.
    There is some hackery needed for the LD_PRELOAD shared library,
    but apart from that there is nothing special and it is a large gain in
    portability and standardization.
    
    The spec file is modified as little as is needed to properly build
    the RPMs.

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 rm -rf autom4te.cache
-rm -f configure config.h.in libiscsi.pc
+rm -f depcomp aclocal.m4 missing config.guess config.sub install-sh
+rm -f configure config.h.in config.h.in~ m4/libtool.m4 m4/lt*.m4 Makefile.in
 
-IPATHS="-I ./include -I ../include"
-
-autoheader $IPATHS || exit 1
-autoconf $IPATHS || exit 1
-
+autoreconf -fvi
+(cd m4 && for i in libtool.m4 lt*.m4; do
+  echo /$i
+done) > m4/.gitignore
 rm -rf autom4te.cache
 
 echo "Now run ./configure and then make."
