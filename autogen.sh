23b40111fcfac5aa4824bd309f468db190be918e
Michael Tokarev <mjt@tls.msk.ru>
2012-05-13 14:59:54 +0400

    ignore m4/ directory and (re)create it in autogen.sh
    m4/ is used by auto* tools to store (mostly libtool)
    generated files.  Without it being present, autoreconf&Co
    does not work, and with it present, git mentions untracked
    files.  Fix both.
    
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@
 rm -rf autom4te.cache
 rm -f depcomp aclocal.m4 missing config.guess config.sub install-sh
 rm -f configure config.h.in config.h.in~ m4/libtool.m4 m4/lt*.m4 Makefile.in
-
+mkdir -p m4
 autoreconf -fvi
 (cd m4 && for i in libtool.m4 lt*.m4; do
   echo /$i
