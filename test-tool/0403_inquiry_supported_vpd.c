c195348e4d3f654736ad38fb08f9982a5d04fadb
Arne Redlich <arne.redlich@googlemail.com>
2012-10-31 16:12:21 +0100

    T0403_inquiry_supported_vpd: fix compiler warnings
    Remove unused labels and mark unused parameter as such.
    
    Signed-off-by: Arne Redlich <arne.redlich@googlemail.com>

--- test-tool/0403_inquiry_supported_vpd.c
+++ test-tool/0403_inquiry_supported_vpd.c
@@ -1,16 +1,16 @@
-/* 
+/*
    Copyright (C) 2012 by Ronnie Sahlberg <ronniesahlberg@gmail.com>
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
@@ -22,8 +22,9 @@
 #include "scsi-lowlevel.h"
 #include "iscsi-test.h"
 
-int T0403_inquiry_supported_vpd(const char *initiator, const char *url, int data_loss, int show_info)
-{ 
+int T0403_inquiry_supported_vpd(const char *initiator, const char *url,
+				int data_loss _U_, int show_info)
+{
 	struct iscsi_context *iscsi;
 	struct scsi_task *task;
 	struct scsi_inquiry_supported_pages *inq;
@@ -51,10 +52,7 @@ int T0403_inquiry_supported_vpd(const char *initiator, const char *url, int data
 		return -1;
 	}
 
-
 	ret = 0;
-
-
 
 	printf("Read SUPPORTED VPD data ... ");
 	/* See how big this inquiry data is */
@@ -95,7 +93,6 @@ int T0403_inquiry_supported_vpd(const char *initiator, const char *url, int data
 	}
 	printf("[OK]\n");
 
-test2:
 	printf("Verify we have all mandatory SPC VPD pages:\n");
 	for (i = 0; i < sizeof(required_spc_pages) / sizeof(enum scsi_inquiry_pagecode); i++) {
 		printf("Verify the target supports page 0x%02x ... ", required_spc_pages[i]);
@@ -113,8 +110,6 @@ test2:
 		}
 	}
 
-
-test3:
 	scsi_free_scsi_task(task);
 
 finished:
