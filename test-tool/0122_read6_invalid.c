34b25f94c13b8c745395e3f79fd0209fa58f5dae
Arne Redlich <arne.redlich@googlemail.com>
2012-10-31 15:53:08 +0100

    0122_read6_invalid.c: add missing include
    Signed-off-by: Arne Redlich <arne.redlich@googlemail.com>

--- test-tool/0122_read6_invalid.c
+++ test-tool/0122_read6_invalid.c
@@ -1,16 +1,16 @@
-/* 
+/*
    Copyright (C) 2010 by Ronnie Sahlberg <ronniesahlberg@gmail.com>
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
@@ -19,11 +19,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "iscsi.h"
+#include "iscsi-private.h"
 #include "scsi-lowlevel.h"
 #include "iscsi-test.h"
 
 int T0122_read6_invalid(const char *initiator, const char *url, int data_loss _U_, int show_info)
-{ 
+{
 	struct iscsi_context *iscsi;
 	struct scsi_task *task;
 	struct iscsi_data data;
