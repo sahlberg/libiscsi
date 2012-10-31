af33ca36ff33a84427ac40c6614319f44e93cb4d
Arne Redlich <arne.redlich@googlemail.com>
2012-10-31 16:50:38 +0100

    T1031_unsolicited_data_out: mark unused parameters as such
    Signed-off-by: Arne Redlich <arne.redlich@googlemail.com>

--- test-tool/1031_unsolicited_data_out.c
+++ test-tool/1031_unsolicited_data_out.c
@@ -1,16 +1,16 @@
-/* 
+/*
    Copyright (C) 2012 by Ronnie Sahlberg <ronniesahlberg@gmail.com>
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
@@ -27,7 +27,7 @@
 uint32_t block_size;
 
 static int
-my_iscsi_add_data(struct iscsi_context *iscsi, struct iscsi_data *data,
+my_iscsi_add_data(struct iscsi_context *iscsi _U_, struct iscsi_data *data,
 	       unsigned char *dptr, int dsize, int pdualignment)
 {
 	int len, aligned;
@@ -149,8 +149,9 @@ my_iscsi_allocate_pdu_with_itt_flags(struct iscsi_context *iscsi, enum iscsi_opc
 	return pdu;
 }
 
-int T1031_unsolicited_data_out(const char *initiator, const char *url, int data_loss, int show_info)
-{ 
+int T1031_unsolicited_data_out(const char *initiator, const char *url,
+			       int data_loss _U_, int show_info)
+{
   struct iscsi_context *iscsi, *iscsi2;
 	struct scsi_task *task;
 	struct scsi_readcapacity16 *rc16;
