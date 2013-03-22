d5c5fb83afab255db2bf12a319739944dffe11c7
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2013-03-21 19:26:55 -0700

    We should still be able to login even if the device is reserved.
    TESTUNITREADY returning reservation conflict is no reason to fail the login

--- lib/connect.c
+++ lib/connect.c
@@ -77,6 +77,11 @@ iscsi_testunitready_cb(struct iscsi_context *iscsi, int status,
 		status = 0;
 	}
 
+	/* Dont fail the login just because the medium is reserved */
+	if (status == SCSI_STATUS_RESERVATION_CONFLICT) {
+		status = 0;
+	}
+
 	ct->cb(iscsi, status?SCSI_STATUS_ERROR:SCSI_STATUS_GOOD, NULL,
 	       ct->private_data);
 	scsi_free_scsi_task(task);
