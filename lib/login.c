598621840fe32bc114c7505022b351e4907e11b4
Peter Lieven <pl@kamp.de>
2014-06-13 13:10:18 +0200

    logout: cork socket after logout has been sent
    Signed-off-by: Peter Lieven <pl@kamp.de>

--- lib/login.c
+++ lib/login.c
@@ -1154,6 +1154,7 @@ iscsi_logout_async(struct iscsi_context *iscsi, iscsi_command_cb cb,
 
 	pdu->callback     = cb;
 	pdu->private_data = private_data;
+	pdu->flags |= ISCSI_PDU_CORK_WHEN_SENT;
 
 	if (iscsi_queue_pdu(iscsi, pdu) != 0) {
 		iscsi_set_error(iscsi, "Out-of-memory: failed to queue iscsi "
