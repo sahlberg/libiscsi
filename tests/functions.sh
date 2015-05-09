86ce6a0ccd190bd241975631ee2768389c819d6e
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2015-05-09 15:39:45 -0700

    TESTS: tgtd now had TGT_IPC_SOCKET so lets use it
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- tests/functions.sh
+++ tests/functions.sh
@@ -1,15 +1,21 @@
-TGTD=tgtd
-TGTADM=tgtadm
+export TGT_IPC_SOCKET=`pwd`/tgtd.socket 
+
+TGTD="tgtd"
+TGTADM="tgtadm"
 TGTLUN=`pwd`/100M
-TGTPORTAL=127.0.0.1:3260
+TGTPORTAL=127.0.0.1:3269
 
 IQNTARGET=iqn.libiscsi.unittest.target
 IQNINITIATOR=iqn.libiscsi.unittest.initiator
 TGTURL=iscsi://${TGTPORTAL}/${IQNTARGET}/1
 
 start_target() {
+    # in case we have one still running from a previous run
+    ${TGTADM} --op delete --force --mode target --tid 1 2>/dev/null
+    ${TGTADM} --op delete --mode system 2>/dev/null
     # Setup target
     echo "Starting iSCSI target"
+    #${TGTD} --iscsi -f portal=${TGTPORTAL},${1} 2>/dev/null &
     ${TGTD} --iscsi portal=${TGTPORTAL},${1}
     sleep 1
     ${TGTADM} --op new --mode target --tid 1 -T ${IQNTARGET}
