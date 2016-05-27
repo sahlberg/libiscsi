e3df0bbf96c09b73d6b3bd77707d100d71cc3c56
Roy Shterman <roysh@mellanox.com>
2016-05-27 18:47:16 +0300

    Libiscsi: Adding queue pdu function to transport abstraction
    include/iscsi-private: adding queue_pdu in transport function pointers
                           struct
    include/iscsi: declaration of tcp_queue_pdu function
    
    socket: adding queue_pdu function to transport initialization
    
    all_library: changing iscsi_queue_pdu into iscsi->t->queue_pdu
    
    Signed-off-by: Roy Shterman <roysh@mellanox.com>

--- lib/discovery.c
+++ lib/discovery.c
@@ -80,7 +80,7 @@ iscsi_discovery_async(struct iscsi_context *iscsi, iscsi_command_cb cb,
 	pdu->callback     = cb;
 	pdu->private_data = private_data;
 
-	if (iscsi_queue_pdu(iscsi, pdu) != 0) {
+	if (iscsi->t->queue_pdu(iscsi, pdu) != 0) {
 		iscsi_set_error(iscsi, "Out-of-memory: failed to queue iscsi "
 				"text pdu.");
 		iscsi_free_pdu(iscsi, pdu);
