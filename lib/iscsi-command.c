f475436c5abf4312779f6083e6c76dfe05fb095e
David Disseldorp <ddiss@suse.de>
2017-01-03 19:33:01 +0100

    Libiscsi: add support for RECEIVE COPY RESULTS
    Build on existing scsi_cdb_receive_copy_results() functionality. This
    request is most commonly used to determine target-side EXTENDED COPY
    operational parameters.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- lib/iscsi-command.c
+++ lib/iscsi-command.c
@@ -2612,6 +2612,29 @@ iscsi_report_supported_opcodes_task(struct iscsi_context *iscsi, int lun,
 }
 
 struct scsi_task *
+iscsi_receive_copy_results_task(struct iscsi_context *iscsi, int lun,
+				int sa, int list_id, int alloc_len,
+				iscsi_command_cb cb, void *private_data)
+{
+	struct scsi_task *task;
+
+	task = scsi_cdb_receive_copy_results(sa, list_id, alloc_len);
+	if (task == NULL) {
+		iscsi_set_error(iscsi, "Out-of-memory: Failed to create "
+				"RECEIVE COPY RESULTS cdb.");
+		return NULL;
+	}
+
+	if (iscsi_scsi_command_async(iscsi, lun, task, cb,
+				     NULL, private_data) != 0) {
+		scsi_free_scsi_task(task);
+		return NULL;
+	}
+
+	return task;
+}
+
+struct scsi_task *
 iscsi_scsi_get_task_from_pdu(struct iscsi_pdu *pdu)
 {
 	return pdu->scsi_cbdata.task;
