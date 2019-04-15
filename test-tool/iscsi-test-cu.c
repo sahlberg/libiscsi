sahlberg/libiscsi

commit 16435a917d671c3fca4ef4a3372eaf8db4492e2b

iscsi-test-cu: Improve persistent reservation clearing
Clear persistent reservations before sending the first SCSI command to the
target to avoid that that command fails due to persistent reservations left
behind by a previous run of the test tool.

Avoid that a subsequent test fails if a test did not remove the persistent
reservations it obtained.

Make sure that clear_pr() returns -1 if clearing persistent reservations
fails.

Signed-off-by: Bart Van Assche <bvanassche@acm.org>

test-tool/iscsi-test-cu.c
@@ -787,8 +787,12 @@ static int clear_pr(struct scsi_device *sdev)
 
         res = 0;
         for (i = 0; i < rk->num_keys; i++) {
-                prout_register_and_ignore(sdev, rk->keys[i]);
-                prout_register_key(sdev, 0, rk->keys[i]);
+                res = prout_register_and_ignore(sdev, rk->keys[i]);
+                if (res)
+                        break;
+                res = prout_register_key(sdev, 0, rk->keys[i]);
+                if (res)
+                        break;
         }
 
         scsi_free_scsi_task(pr_task);
@@ -857,6 +861,7 @@ suite_cleanup(void)
         for (i = 0; i < mp_num_sds; i++) {
                 if (mp_sds[i]->iscsi_url) {
                         if (mp_sds[i]->iscsi_ctx) {
+                                clear_pr(mp_sds[i]);
                                 iscsi_logout_sync(mp_sds[i]->iscsi_ctx);
                                 iscsi_destroy_context(mp_sds[i]->iscsi_ctx);
                                 mp_sds[i]->iscsi_ctx = NULL;
@@ -1241,6 +1246,10 @@ main(int argc, char *argv[])
                                 "Failed to connect to SCSI device %d\n", i);
                         goto err_sds_free;
                 }
+                if (clear_pr(mp_sds[i]) < 0) {
+                        printf("One or more persistent reservations keys have been registered\n");
+                        return -1;
+                }
         }
 
         if (mp_num_sds > 1) {
@@ -1438,11 +1447,6 @@ main(int argc, char *argv[])
                 readonly = !!(ms->device_specific_parameter & 0x80);
         }
         scsi_free_scsi_task(task);
-
-        if (clear_pr(sd) < 0) {
-                printf("One or more persistent reservations keys have been registered\n");
-                return -1;
-        }
 
         /* BLKSECTGET for /dev/sg* is a shitshow under linux.
          * Even 4.2 kernels return number of bytes instead of number

commit 1af3314e2bd4d62bafebf7131d5038809d2b8af3

test-tool: fix MPIO clear_pr() behaviour
clear_pr() can be called before connect_scsi_device() has been invoked
on each MPIO sdev. This results in a NULL pointer dereference in the
send_scsi_command() MPIO NOP-responder code-path.

Fix this by invoking clear_pr() after the MPIO connect_scsi_device()
loop has run. clear_pr() also only needs to be invoked via a single
path, so do so via the first path only.

Link: https://github.com/sahlberg/libiscsi/issues/289
Fixes: 16435a9 ("iscsi-test-cu: Improve persistent reservation clearing")
Signed-off-by: David Disseldorp <ddiss@suse.de>

test-tool/iscsi-test-cu.c
@@ -1245,11 +1245,12 @@ main(int argc, char *argv[])
                                 "Failed to connect to SCSI device %d\n", i);
                         goto err_sds_free;
                 }
-                if (clear_pr(mp_sds[i]) < 0) {
-                        printf("One or more persistent reservations keys have been registered\n");
-                        return -1;
-                }
         }
+
+	if (clear_pr(sd) < 0) {
+		printf("One or more persistent reservations keys have been registered\n");
+		goto err_sds_free;
+	}
 
         if (mp_num_sds > 1) {
                 /* check that all multipath sds identify as the same LU */