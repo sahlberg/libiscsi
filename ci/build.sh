8b4670db3e2e0da48d332a14e8eac0a45cbcb75c
Bart Van Assche <bvanassche@acm.org>
2021-07-17 19:50:35 -0700

    ci: First phase of adding MingW support

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+case "$(uname)" in
+    MSYS*|MINGW*)
+	pacman --noconfirm --remove mingw-w64-x86_64-gcc-ada
+	pacman --noconfirm --remove mingw-w64-x86_64-gcc-fortran
+	pacman --noconfirm --remove mingw-w64-x86_64-gcc-libgfortran
+	pacman --noconfirm --remove mingw-w64-x86_64-gcc-objc
+	pacman --noconfirm --sync --refresh
+	pacman --noconfirm --sync --needed diffutils
+	pacman --noconfirm --sync --needed make
+	pacman --noconfirm --sync --needed mingw-w64-x86_64-gcc
+	pacman --noconfirm --sync --needed mingw-w64-x86_64-cunit
+	export PATH="/mingw64/bin:$PATH"
+	;;
+esac
+
 ./autogen.sh &&
     ./configure --enable-manpages --enable-test-tool --enable-tests \
 		--enable-examples &&
