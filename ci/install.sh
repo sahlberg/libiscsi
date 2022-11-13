c23545c604241689f69ac1128260fad2d0bd96dc
Bart Van Assche <bvanassche@acm.org>
2022-11-12 17:34:21 -0800

    ci: Fix the Appveyor Linux and Windows builds

--- ci/install.sh
+++ ci/install.sh
@@ -3,9 +3,13 @@
 case "$(uname)" in
     Linux)
 	packages=(
+	    autoconf
+	    automake
+	    docbook-xsl
 	    libcunit1-dev
 	    libgcrypt20-dev
 	    librdmacm-dev
+	    libtool
 	    xsltproc
 	)
 	for p in "${packages[@]}"; do
@@ -19,10 +23,15 @@ case "$(uname)" in
 	pacman --noconfirm --remove mingw-w64-x86_64-gcc-fortran
 	pacman --noconfirm --remove mingw-w64-x86_64-gcc-libgfortran
 	pacman --noconfirm --remove mingw-w64-x86_64-gcc-objc
+	pacman --noconfirm --remove mingw-w64-x86_64-libgccjit
 	pacman --noconfirm --sync --refresh
+	pacman --noconfirm --sync --needed autoconf
+	pacman --noconfirm --sync --needed automake
 	pacman --noconfirm --sync --needed diffutils
+	pacman --noconfirm --sync --needed docbook-xsl
+	pacman --noconfirm --sync --needed libtool
 	pacman --noconfirm --sync --needed make
-	pacman --noconfirm --sync --needed mingw-w64-x86_64-gcc
 	pacman --noconfirm --sync --needed mingw-w64-x86_64-cunit
+	pacman --noconfirm --sync --needed mingw-w64-x86_64-gcc
 	;;
 esac
