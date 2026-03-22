b2b05b6fbe98dc344c8498bb5c42a10aebc61c0e
Brad Smith <brad@comstyle.com>
2026-03-22 00:12:47 -0400

    Add thread id retrieval for FreeBSD/DragonFly, OpenBSD and NetBSD

--- lib/multithreading.c
+++ lib/multithreading.c
@@ -144,12 +144,27 @@ int iscsi_mt_sem_wait(libiscsi_sem_t* sem)
 #include <signal.h>
 #include <unistd.h>
 #include <sys/syscall.h>
+#if defined(__FreeBSD__) || defined(__DragonFly__)
+#include <pthread_np.h>
+#endif
+#if defined(__NetBSD__)
+#include <lwp.h>
+#endif
 
 iscsi_tid_t iscsi_mt_get_tid(void)
 {
 #ifdef HAVE_PTHREAD_THREADID_NP
         iscsi_tid_t tid;
         pthread_threadid_np(NULL, &tid);
+        return tid;
+#elif defined(__FreeBSD__) || defined(__DragonFly__)
+        int tid = pthread_getthreadid_np();
+        return tid;
+#elif defined(__OpenBSD__)
+        pid_t tid = getthrid();
+        return tid;
+#elif defined(__NetBSD__)
+        lwpid_t tid = _lwp_self();
         return tid;
 #elif defined(SYS_gettid)
         pid_t tid = syscall(SYS_gettid);
